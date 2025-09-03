5dd331872472168ddf5eca5eb095cfb0daaf68bb
pvasbob <pvasbob@gmail.com>
2025-09-03 12:44:14 -0400

    product-container generated by .js in manga.js file

---
+++ scripts/manga-source.js
@@ -0,0 +1,163 @@
+export const products = [
+  {
+    image: "../imgs/Yusuke-1.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/Yusuke-3.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/Yusuke.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Emiya.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Genkai.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Gilgamesh.jpeg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Gohan.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/GonKillua-1.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/GonKillua.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Hiei.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Hiei-2.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Hiei-3.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Hiei-4.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Itsuki.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Kakashi.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Killua.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Koenma.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Kuruma.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/SaberEmiya.avif",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Shigure.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+
+  {
+    image: "../imgs/Shinobu.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/sss.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/Trunks.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+  {
+    image: "../imgs/Yuji.jpg",
+    name: "Emiya",
+    rating: { stars: 4.5, count: 87 },
+    priceCents: 1090,
+  },
+];
